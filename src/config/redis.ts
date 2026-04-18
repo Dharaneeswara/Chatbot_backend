fe1dae379ab7e86ded5d42f814f1b91eb243e614
Dharaneeswaran <dharaneeswar7@gmail.com>
2026-04-18 10:04:06 +0530

    first commit

---
+++ src/config/redis.ts
@@ -0,0 +1,15 @@
+import { createClient } from "redis";
+import { env } from "./env.js";
+import { logger } from "./logger.js";
+
+export const redisClient = createClient({
+  url: env.REDIS_URL,
+});
+
+redisClient.on("error", (err) => logger.error(`Redis error: ${err}`));
+redisClient.on("ready", () => logger.info("Redis is ready to use"));
+redisClient.on("connect", () => logger.info("Redis connected successfully!"));
+
+export const connectRedis = async () => {
+  await redisClient.connect();
+};
